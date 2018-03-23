f48df6b12394d1501b9e73c0d8a7534db8f7f1e5
sundh <sundhakan@gmail.com>
2018-03-23 12:21:45 -0500

    Copy eclipse WS

---
+++ mywebapp/src/main/java/login.java
@@ -0,0 +1,29 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username1=(String)request.getParameter("username");
+		String password1=(String)request.getParameter("password");
+		
+		System.out.println(username1);
+		System.out.println(password1);
+		
+		System.out.println("try");
+		System.out.println("testee");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+}
---
+++ mywebapp/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h2>Hello World..???!!!555</h2>
+
+<form name="loginForm" method="get" action="login">
+Username:<input type="text" name="username"> 
+</br>
+</br>
+Password: <input type="password" name="password">
+</br>
+</br>
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
