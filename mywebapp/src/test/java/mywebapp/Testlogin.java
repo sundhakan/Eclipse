b964fce62d5197dfd5c830172d5749f21cd43c91
sundh <sundhakan@gmail.com>
2018-03-24 11:59:14 -0500

    Adding selinium

--- mywebapp/src/test/java/mywebapp/Testlogin.java
+++ mywebapp/src/test/java/mywebapp/Testlogin.java
@@ -1,10 +1,18 @@
 package mywebapp;
 
 import junit.framework.TestCase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Testlogin extends TestCase {
 
 	public void testDoGetHttpServletRequestHttpServletResponse() {
+		
+		//System.setProperty("webdriver.gecko.driver","D:\\Java\\Practice\\Practice Workspaces\\Seleni\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
 		//fail("Not yet implemented");
 	}
 
