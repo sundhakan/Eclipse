6f1e6f9f4a10fb764928bb837b2037f9a37f539a
sundh <sundhakan@gmail.com>
2018-03-24 20:34:46 -0500

    included TestNG ,and test case for the same

---
+++ mywebapp/src/test/java/mywebapp/NewTest.java
@@ -0,0 +1,25 @@
+package mywebapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String driverPath = "geckodriver.exe";
+    public WebDriver driver ; 
+     
+  @Test
+  public void verifyHomepageTitle() {
+       
+      System.out.println("launching firefox browser"); 
+      System.setProperty("webdriver.gecko.driver", driverPath);
+      driver = new FirefoxDriver();
+      driver.get(baseUrl);
+      String expectedTitle = "Welcome: Mercury Tours";
+      String actualTitle = driver.getTitle();
+      Assert.assertEquals(actualTitle, expectedTitle);
+      driver.close();
+  }
+}
--- mywebapp/src/test/java/mywebapp/Testlogin.java
+++ mywebapp/src/test/java/mywebapp/Testlogin.java
@@ -1,6 +1,10 @@
 package mywebapp;
 
 import junit.framework.TestCase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.By.ByName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -13,6 +17,9 @@ public class Testlogin extends TestCase {
 		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://localhost:8080/mywebapp/");
+		driver.findElement(By.name("username")).sendKeys("hahahahhhahahaha");
+		driver.findElement(By.name("password")).sendKeys("dhakan007");
+		driver.findElement(By.name("submit")).click();
 		System.out.println("Yurekkaaaaaaa...!!!!!");
 		//fail("Not yet implemented");
 	}
